e7d41ebaed8d4693cc0067eb7ace659257fa2b42
Manuel Argüelles <marguelles.dev@gmail.com>
2024-06-18 08:01:35 +0700

    s32: s32k3: skip STM header to use native driver
    Signed-off-by: Manuel Argüelles <marguelles.dev@gmail.com>

--- s32/drivers/s32k3/BaseNXP/header/S32K344.h
+++ s32/drivers/s32k3/BaseNXP/header/S32K344.h
@@ -77,7 +77,6 @@
 #include "S32K344_SIRC.h"
 #include "S32K344_SIUL2.h"
 #include "S32K344_STCU.h"
-#include "S32K344_STM.h"
 #include "S32K344_SWT.h"
 #include "S32K344_SXOSC.h"
 #include "S32K344_TEMPSENSE.h"
